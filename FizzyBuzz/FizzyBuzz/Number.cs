a8acc10a66605bee3790a79ef94b9f4c38742b33
george <george@grgilbert.com>
2017-01-14 12:00:10 -0500

    Basic implementation

---
+++ FizzyBuzz/FizzyBuzz/Number.cs
@@ -0,0 +1,35 @@
+﻿namespace FizzyBuzz
+{
+	public class Number
+	{
+		public Number(int intitialize)
+		{
+			_value = intitialize;
+		}
+
+		public bool IsMod3()
+		{
+			return _value % 3 == 0;
+		}
+
+		public bool IsMod5()
+		{
+			return _value % 5 == 0;
+		}
+
+		public int Value
+		{
+			get
+			{
+				return _value;
+			}
+
+			set
+			{
+				_value = value;
+			}
+		}
+
+		protected int _value;
+	}
+}
---
+++ FizzyBuzz/FizzyBuzz/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace FizzyBuzz
+{
+	public class Program
+	{
+		static void Main(string[] args)
+		{
+			for (Number number = new Number(1); number.Value <= 100; number.Value++)
+			{
+				if (number.IsMod3() && number.IsMod5())
+				{
+					Console.WriteLine("FizzBuzz");
+				}
+				else if (number.IsMod3())
+				{
+					Console.WriteLine("Fizz");
+				}
+				else if (number.IsMod5())
+				{
+					Console.WriteLine("Buzz");
+				}
+				else
+				{
+					Console.WriteLine(number.Value);
+				}
+			}
+
+			Console.Read();
+		}
+	}
+}
